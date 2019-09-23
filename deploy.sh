969e99e594091da8b5808ef3ced249ea5a277c6d
ermiry <stumpestro@ermiry.com>
2019-09-22 20:26:30 -0500

    Added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while getopts u:p:s: option
+    do
+    case "${option}"
+        in
+        u) USER=${OPTARG};;
+        p) PSWD=${OPTARG};;
+        s) SERVER=${OPTARG};;
+    esac
+done
+
+docker build -t ermiry/black-cerver:production .
+echo ${PSWD} | docker login -u ${USER} --password-stdin
+docker push ermiry/black-cerver:production
+
+chmod 600 deploy_key && ssh -o StrictHostKeyChecking=no -i deploy_key ermiry@${SERVER} ./start.sh
